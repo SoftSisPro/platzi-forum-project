1cc5367491a729c50bb92b3be5c1a04b234fcdd1
SoftSisPro <softsispro@gmail.com>
2024-09-24 18:56:44 -0500

    Filtrar por categoria con liviwire

--- app/Livewire/ShowThreads.php
+++ app/Livewire/ShowThreads.php
@@ -9,13 +9,23 @@ use Livewire\Component;
 class ShowThreads extends Component
 {
     public $search = "";
+    public $category = "";
+
+    public function filterByCategory($category)
+    {
+        $this->category = $category;
+    }
 
     public function render()
     {
+        # Get all categories
         $categories = Category::get();
 
         $threads = Thread::query();
         $threads -> where('title', 'like', "%$this->search%"); //- search by title
+        if($this->category) {
+            $threads -> where('category_id', $this->category); // - filter by category
+        }
         $threads -> withCount('replies'); // - count the number of replies
         $threads -> latest(); // - order by latest
 
