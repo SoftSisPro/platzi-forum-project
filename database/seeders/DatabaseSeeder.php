3036b831d7aaa452b903ff44c890a99f13b7dabc
SoftSisPro <softsispro@gmail.com>
2024-09-24 18:12:54 -0500

    Creamos Tabla Respuesta

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Reply;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //- Creamos 10 usuarios
         User::factory()->create(['email' => 'jacajali@gmail.com']);
         User::factory(9)->create();
 
+        //- Creamos 10 categorías, cada una con 20 hilos
         Category::factory(10)
             ->hasThreads(20)
             ->create();
+
+        //- Creamos 400 respuestas
+        Reply::factory(400)->create();
     }
 }
