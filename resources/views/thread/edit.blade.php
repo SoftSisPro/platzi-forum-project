fdf8afabdca4337b4ec0d733fa3960866cf95529
SoftSisPro <softsispro@gmail.com>
2024-09-25 20:52:56 -0500

    Campos del formulario de preguntar

--- resources/views/thread/edit.blade.php
+++ resources/views/thread/edit.blade.php
@@ -3,7 +3,7 @@
         <div class="rounded-md bg-gradient-to-r from-slate-700 to-slate-900 mb-4">
             <div class="p-4">
                 <h2 class="mb-4 text-xl font-semibold text-white/90">
-                    Editar pregunta
+                    <i class="fas fa-pen mr-1"></i> Editar Pregunta:
                 </h2>
                 <form action="{{ route('threads.update', $thread) }}" method="POST">
                     @csrf
--- resources/views/thread/form.blade.php
+++ resources/views/thread/form.blade.php
@@ -1,4 +1,31 @@
 <div>
+    <select
+        name="category_id"
+        class="bg-slate-700 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs capitalize mb-4"
+    >
+        <option value="">Seleccionar Categoría</option>
+        @foreach ($categories as $category)
+            <option
+                value="{{ $category->id }}"
+                @if ($thread->category_id == $category->id)
+                    selected
+                @endif
+            >{{ $category->name }}</option>
+        @endforeach
+    </select>
+    
+    <input
+        type="text"
+        name="title"
+        placeholder="Título de la pregunta"
+        class="bg-slate-700 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4"
+        value="{{ $thread->title }}"
+    >
 
-    formulario
+    <textarea
+        name="body"
+        rows="10"
+        placeholder="Escribe tu pregunta"
+        class="bg-slate-700 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs mb-4"
+    >{{ $thread->body }}</textarea>
 </div>
