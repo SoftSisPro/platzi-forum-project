783208de17a942d5f99e829c993fe051181fbeef
SoftSisPro <softsispro@gmail.com>
2024-09-25 19:27:47 -0500

    Editar una respuesta

--- app/Livewire/ShowReply.php
+++ app/Livewire/ShowReply.php
@@ -10,11 +10,40 @@ class ShowReply extends Component
     public Reply $reply;
     public $body ="";
     public $is_creating = false;
+    public $is_editing = false;
+
+    public function updatedIsCreating()
+    {
+        $this->is_editing= false; # close the edit form
+        $this->body = ""; # clear the body
+    }
+
+    public function updatedIsEditing()
+    {
+        $this->is_creating = false; # close the create form
+        $this->body = $this->reply->body; # set the body to the current reply body
+    }
+
+    public function updateReply()
+    {
+        //- validate
+        $this->validate([
+            'body' => 'required',
+        ]);
+
+        // Crear una nueva respuesta
+        $this->reply->update([
+            'body' => $this->body,
+        ]);
+
+        //- refresh
+        $this->is_editing = false; # close the edit form
+    }
 
     public function postChild()
     {
-        if(!is_null($this->reply->reply_id)) return;
-        
+        if(!is_null($this->reply->reply_id)) return; # only the first level replies can have children
+
         //- validate
         $this->validate([
             'body' => 'required',
--- resources/views/livewire/show-reply.blade.php
+++ resources/views/livewire/show-reply.blade.php
@@ -9,23 +9,36 @@
                     {{ $reply->user->name }}
                     <span class="text-white/80">{{ $reply->created_at->diffForHumans() }}</span>
                 </p>
-                <p class="text-white/60 text-xs">{{ $reply->body }}</p>
+                <!-- Editar -->
+                @if($is_editing)
+                    <form wire:submit.prevent="updateReply" class="mt-4">
+                        <input
+                            type="text"
+                            placeholder="Escribe tu respuesta"
+                            class="bg-slate-700 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs"
+                            wire:model.defer="body"
+                        >
+                    </form>
+                @else
+                    <p class="text-white/60 text-xs">{{ $reply->body }}</p>
+                @endif
+
                 <!-- formulario -->
                 @if($is_creating)
-                <form wire:submit.prevent="postChild" class="mt-4">
-                    <input
-                        type="text"
-                        placeholder="Escribe tu respuesta"
-                        class="bg-slate-700 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs"
-                        wire:model.defer="body"
-                    >
-                </form>
+                    <form wire:submit.prevent="postChild" class="mt-4">
+                        <input
+                            type="text"
+                            placeholder="Escribe tu respuesta"
+                            class="bg-slate-700 border-1 border-slate-900 rounded-md w-full p-3 text-white/60 text-xs"
+                            wire:model.defer="body"
+                        >
+                    </form>
                 @endif
                 <p class="mt-4 text-white/60 text-xs flex gap-2 justify-end">
                     @if(is_null($reply->reply_id))
                         <a href="#" wire:click.prevent="$toggle('is_creating')" class="hover:text-white">Responder</a>
                     @endif
-                    <a href="" class="hover:text-white">Editar</a>
+                    <a href="#" wire:click.prevent="$toggle('is_editing')" class="hover:text-white">Editar</a>
                 </p>
             </div>
         </div>
